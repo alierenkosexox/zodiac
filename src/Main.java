2d5dcf1b98b06a945f64d86b39ea3e90cf1c001d
Ali Eren KÖSE <alierenkose@gmail.com>
2024-02-23 04:46:34 +0300

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,59 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        int birthYear,zodiac;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Doğum yılınızı giriniz : ");
+        birthYear = input.nextInt();
+
+        zodiac = (birthYear % 12);
+        String horoscopce;
+        if (zodiac==0) {
+            horoscopce="Maymun";
+            System.out.println("Çin zodyağı burcunu : "+ horoscopce);
         }
+        if (zodiac==1) {
+            horoscopce="Horoz";
+            System.out.println("Çin zodyağı burcunu : "+ horoscopce);
+        }
+        if (zodiac==2) {
+            horoscopce="Köpek";
+            System.out.println("Çin zodyağı burcunu : "+ horoscopce);
+        }
+        if (zodiac==3) {
+            horoscopce="Domuz";
+            System.out.println("Çin zodyağı burcunu : "+ horoscopce);
+        }if (zodiac==4) {
+            horoscopce="Fare";
+            System.out.println("Çin zodyağı burcunu : "+ horoscopce);
+        }if (zodiac==5) {
+            horoscopce="Öküz";
+            System.out.println("Çin zodyağı burcunu : "+ horoscopce);
+        }if (zodiac==6) {
+            horoscopce="Kaplan";
+            System.out.println("Çin zodyağı burcunu : "+ horoscopce);
+        }if (zodiac==7) {
+            horoscopce="Tavşan";
+            System.out.println("Çin zodyağı burcunu : "+ horoscopce);
+        }if (zodiac==8) {
+            horoscopce="Ejderha";
+            System.out.println("Çin zodyağı burcunu : "+ horoscopce);
+        }if (zodiac==9) {
+            horoscopce="Yılan";
+            System.out.println("Çin zodyağı burcunu : "+ horoscopce);
+        }if (zodiac==10) {
+            horoscopce="At";
+            System.out.println("Çin zodyağı burcunu : "+ horoscopce);
+        }if (zodiac==11) {
+            horoscopce="Koyun";
+            System.out.println("Çin zodyağı burcunu : "+ horoscopce);
+        }
+
+
     }
+
 }
